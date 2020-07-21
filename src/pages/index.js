e94e5697598dd9436b02d1b78c1e335ed7543d7b
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-07-21 14:36:51 -0700

    na

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,66 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import "../components/customs.scss"
 const IndexPage = () => (
-    <Layout>
-        <section class="section level">
-            <div class="container">
-                <div class="columns is-vcentered is-centered">
-                    <div class="column is-half">
-                        <h1 class="is-size-2">Get in contact</h1>
-                        <form
-                            name="contact"
-                            method="post"
-                            data-netlify="true"
-                            netlify-honeypot="bot-field"
-                        >
-                            <div class="field">
-                                <label class="label">
-                                    Your Name:
-                                    <input
-                                        class="input"
-                                        type="text"
-                                        name="name"
-                                    />
-                                </label>
-                            </div>
-                            <div class="field">
-                                <label class="label">
-                                    Your Email:
-                                    <input
-                                        class="input"
-                                        type="email"
-                                        name="email"
-                                    />
-                                </label>
-                            </div>
-                            <div class="field">
-                                <label class="label">
-                                    Message:
-                                    <textarea
-                                        class="textarea"
-                                        name="message"
-                                    ></textarea>
-                                </label>
-                            </div>
-                            <div class="field">
-                                <button
-                                    class="button is-primary is-medium"
-                                    type="submit"
-                                >
-                                    Send
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <SEO title="Contact form" />
-    </Layout>
+<form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+>
+<input
+    class="input"
+    type="text"
+    placeholder='Your Name"
+    name="name"
+/>
+    <button class="button is-primary is-medium">
+            Send
+        </button>
+</form>
 )
 
 export default IndexPage
