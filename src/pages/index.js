1ad7979aaba16b9795d81f4d24bb035562b36054
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-07-21 14:44:32 -0700

    na

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,35 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import "../components/customs.scss"
 const IndexPage = () => (
-<form
-    name="contact"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
->
-<input
-    class="input"
-    type="text"
-    placeholder="Your Name"
-    name="name"
-/>
-    <button class="button is-primary is-medium">
-            Send
-        </button>
-</form>
+    <Layout>
+        <section class="section level">
+            <div class="container">
+                <div class="columns is-vcentered is-centered">
+                    <div class="column is-half">
+                        <h1 class="is-size-2">Get in contact</h1>
+                        <form
+                            name="contact"
+                            method="post"
+                            data-netlify="true"
+                            data-netlify-honeypot="bot-field"
+                        >
+                        <input
+                            class="input"
+                            type="text"
+                            placeholder="Your Name"
+                            name="name"
+                        />
+                            <button  class="button is-primary is-medium"
+                                >
+                                    Send
+                                </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <SEO title="Contact form" />
+    </Layout>
 )
 
 export default IndexPage
